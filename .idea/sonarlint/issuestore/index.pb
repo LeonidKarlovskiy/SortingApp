0853653d23947aaca1e7424869f892e2dec0e6e1
Leonid Karlovskii <karlovskiyleo@gmail.com>
2023-01-06 18:41:45 +0300

    start

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+R
+".idea/artifacts/SortingApp_jar.xml,1\1\112a6a6a9b69e933d2d1974722b7f5ebb41a25e8
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ src/main/java/com/Leonid/sorting_app/Main.java
@@ -0,0 +1,14 @@
+package com.leonid.sorting_app;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Sorter sorter = new Sorter();
+        System.out.println(Arrays.toString(sorter.convert(args)));
+
+
+    }
+}

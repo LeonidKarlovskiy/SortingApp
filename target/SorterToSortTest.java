5426dc4be575e7abcda65f27cd6ecb1663cd0d9b
Leonid Karlovskii <karlovskiyleo@gmail.com>
2023-01-06 10:31:55 +0300

    start

---
+++ target/SorterToSortTest.java
@@ -0,0 +1,43 @@
+package com.Leonid.SortingApp;
+
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SorterConverterTest {
+
+
+
+    private static Sorter sorter;
+
+
+    @BeforeClass
+    public static void setUp() {
+        sorter = new Sorter();
+
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void noElementsCaseInConvertMethod() {
+        String[] args = {};
+        sorter.convert(args);
+    }
+
+    @Test
+    public void oneElementsCaseInConvertMethod() {
+        String[] args = {"1"};
+        int[] expectedArgs = {1};
+        int[] result = sorter.convert(args);
+        Assert.assertArrayEquals(expectedArgs, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void moreThenTenDigitsCaseInConvertMethod() {
+        String[] args = {"1", "24", "21", "43", "12", "65", "34", "6", "23", "2", "765", "34"};
+        sorter.convert(args);
+    }
+
+
+}
